243b6b7bdc77c099bc35e8b2ceab1b345691c064
marius-rothenbuecher <marius.rothenbuecher@gmail.com>
2017-03-29 21:23:38 +0200

    Add length in bit.

---
+++ PaketJunge/Converters/ObjectToSizeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Windows.Data;
+
+namespace PaketJunge.View.Converters
+{
+    public class ObjectToSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Marshal.SizeOf(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
